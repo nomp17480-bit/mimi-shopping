27ac944279cc52b4d914bb7567b9db6440d04dc8
nomp17480-bit <nomp17480@gmail.com>
2026-09-07 19:49:20 +0100

    Delete server.ts

--- server.ts
+++
@@ -1,1 +0,0 @@
-import {createServerClient} from '@supabase/ssr';import {cookies} from 'next/headers';export async function createClient(){const c=await cookies();return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!,process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,{cookies:{getAll:()=>c.getAll(),setAll:()=>{}}})}
